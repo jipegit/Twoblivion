1437c34346719e3ba71babba32a25ff709307b6b
Jipe <jipedevs@gmail.com>
2013-07-04 00:07:29 +0200

    Version 0.1
    Version 0.1

--- README.md
+++ README.md
@@ -1,9 +1,49 @@
-Twoblivion
-==========
+# Twoblivion
 
 Twoblivion is a free tool to automatically delete your old tweets
 
-Author 
-======
+## Author 
 
-@Jipe_
+Jean-Philippe Teissier - @Jipe_ 
+
+## How to install
+
+Copy all files from Github
+
+## Dependencies
+
+* pip install python-twitter
+
+## How to run
+
+1) Create an application and set TWOBLIVION_CONSUMER_KEY and TWOBLIVION_CONSUMER_SECRET in the source code. See https://dev.twitter.com/ 
+2) Obtain a R/W access token for your account. See https://dev.twitter.com/docs/auth/obtaining-access-tokens
+
+YOUR_ACCESS_TOKEN_SECRET and YOUR_ACCESS_TOKEN can be set in the source code itself or passed as arguments with -k and -s
+
+python twoblivion.py -h
+
+## Changelog
+
+### 0.1
+ * Initial alpha release
+
+## License
+
+Twoblivion
+Copyright (C) 2013 Jean-Philippe Teissier
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Bootstrap have its own GPL compatible licence.
--- twoblivion.py
+++ twoblivion.py
@@ -1,14 +1,168 @@
-########################################################################################################
-#                                                                                                      #
-#   Twoblivion by @Jipe_                                                                               #
-#                                                                                                      #
-#   This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported   #
-#   License.http://creativecommons.org/licenses/by-nc-sa/3.0/                                          #
-#                                                                                                      #
-#                                                                                                      #
-########################################################################################################
+# -*- encoding: utf-8 -*-
+#
+#  Twoblivion
+#  
+#  Author: Jean-Philippe Teissier ( @Jipe_ ) 
+#    
+#  This work is licensed under the GNU General Public License
+#
 
 import optparse
+import os
 import twitter
+from dateutil.parser import *
+from dateutil.relativedelta import *
+from datetime import *
 
->>> api = twitter.Api()
+TWOBLIVION_CONSUMER_KEY = ""
+TWOBLIVION_CONSUMER_SECRET = ""					# Well… not really a secret…
+
+YOUR_ACCESS_TOKEN = ""
+YOUR_ACCESS_TOKEN_SECRET = ""
+
+TEST_USER_ID = ""
+TEST_DATE = "2011 01 01"
+
+Debug = False
+
+def DestroyTweets(TwApi, TweetsToDestroy):
+	""" Destroy the tweets """
+
+	for Tweet in TweetsToDestroy:
+		if Debug: print "TweetID" + str(Tweet) + " destroyed"
+		try:
+			TwApi.DestroyStatus(Tweet)	
+		except twitter.TwitterError as e:
+			print "ERROR: (" + str(e[0][0]['code'])+") " + e[0][0]['message']
+			exit(-1)
+
+def DestroyDM(TwApi, DMToDestroy):
+	""" Destroy the DMs """
+
+	for DM in DMToDestroy:
+		if Debug: print "Direct MessageID" + str(DM) + " destroyed"
+		try:
+			TwApi.DestroyDirectMessage(DM)	
+		except twitter.TwitterError as e:
+			print "ERROR: (" + str(e[0][0]['code'])+") " + e[0][0]['message']
+			exit(-1)
+
+def GetDirectMessagesToDestroy(TwApi, Date):
+	""" Get the list of DM to destroy"""
+
+	TotalDirectMessages = 0
+	DirectMessagesToDestroy = []
+	
+	try:
+		DirectMessages = TwApi.GetDirectMessages(count=200)									#Can't request more than 200 DM
+	except twitter.TwitterError as e:
+		print "ERROR: (" + str(e[0][0]['code'])+") " + e[0][0]['message']
+		exit(-1)
+
+	while len(DirectMessages) > 1:
+		TotalDirectMessages += len(DirectMessages)
+		if Debug: print "Got " + str(TotalDirectMessages) + " Direct Messages"
+		MaxId = DirectMessages[-1].id
+		for DM in DirectMessages:
+			Delta = str(parse(DM.created_at, ignoretz=True) - parse(Date))
+			if Debug: print "ID: " + str(DM.id) + " - Date: " + DM.created_at + " R: " + Delta
+			if Delta[0] == "-":
+				DirectMessagesToDestroy.append(DM.id)
+				if Debug: print str(DM.id) + " (" + DM.created_at + ") will be deleted"
+		try:
+			DirectMessages = TwApi.GetDirectMessages(count=200, max_id=MaxId)
+		except twitter.TwitterError as e:
+			print "ERROR: (" + str(e[0][0]['code'])+") " + e[0][0]['message']
+			exit(-1)
+
+	print "Got " + str(len(DirectMessagesToDestroy)) + " DM to detroy"
+
+	return DirectMessagesToDestroy
+
+
+def GetTweetsToDestroy(TwApi, UserId, Date):
+	""" Get the list of tweets to destroy"""
+	
+	TotalTweets = 0
+	StatusesToDestroy = []
+	
+	try:
+		Statuses = TwApi.GetUserTimeline(user_id=UserId, count=200)						#Can't request more than 200 tweets
+	except twitter.TwitterError as e:
+		print "ERROR: (" + str(e[0][0]['code'])+") " + e[0][0]['message']
+		exit(-1)
+		
+	while len(Statuses) > 1:
+		TotalTweets += len(Statuses)
+		if Debug: print "Got " + str(TotalTweets) + " tweets"
+		MaxId = Statuses[-1].id
+		for S in Statuses:
+			Delta = str(parse(S.created_at, ignoretz=True) - parse(Date))
+			if Debug: print "ID: " + str(S.id) + " - Date: " + S.created_at + " R: " + Delta
+			if Delta[0] == "-":
+				StatusesToDestroy.append(S.id)
+				if Debug: print str(S.id) + " (" + S.created_at + ") will be deleted"
+		try:
+			Statuses = TwApi.GetUserTimeline(user_id=UserId, count=200, max_id=MaxId)
+		except twitter.TwitterError as e:
+			print "ERROR: (" + str(e[0][0]['code'])+") " + e[0][0]['message']
+			exit(-1)
+	
+	print "Got " + str(len(StatusesToDestroy)) + " tweets to detroy"
+	
+	return StatusesToDestroy
+	
+def main():
+	""" Here we go """
+	
+	global Debug
+	TweetsToDestroy = []
+	DirectMessagesToDestroy = []
+	
+	Parser = optparse.OptionParser(usage='usage: %prog [options]')
+	Parser.add_option('-k', '--accesstokenkey', default=False, dest="AccessTokenKey", help='access_token_key')
+	Parser.add_option('-s', '--accesstokensecret', default=False, dest="AccessTokenSecret", help='access_token_secret')
+	Parser.add_option('-u', '--userid', default=False, dest="UserId", help='user_id')
+	Parser.add_option('-d', '--date', default=False, dest="Date", help='date')
+	Parser.add_option('-D', '--debug', action="store_true", default=False, help='debug mode')
+	
+	(options, args) = Parser.parse_args()
+
+	if options.debug:
+		Debug = True
+
+	if options.AccessTokenKey:
+		AcsTokenKey = options.AccessTokenKey
+	else:
+		AcsTokenKey = YOUR_ACCESS_TOKEN
+		
+	if options.AccessTokenSecret:
+		AcsTokenSecret = options.AccessTokenSecret
+	else:
+		AcsTokenSecret = YOUR_ACCESS_TOKEN_SECRET
+	
+	if options.UserId:
+		UserId = options.UserId
+	else:
+		UserId = TEST_USER_ID
+	
+	if options.Date:
+		Date = options.Date
+	else:
+		Date = TEST_DATE
+	
+	TwApi = twitter.Api(consumer_key=TWOBLIVION_CONSUMER_KEY,
+						consumer_secret=TWOBLIVION_CONSUMER_SECRET,
+						access_token_key=AcsTokenKey,
+	 					access_token_secret=AcsTokenSecret)
+	
+	print "Screen Name:" + TwApi.GetUser(UserId).GetScreenName() + " #tweets: " + str(TwApi.GetUser(UserId).statuses_count)
+	
+	TweetsToDestroy = GetTweetsToDestroy(TwApi, UserId, Date)
+	DestroyTweets(TweetsToDestroy)
+	
+	DirectMessagesToDestroy = GetDirectMessagesToDestroy(TwApi, Date)
+	DestroyDM(DirectMessagesToDestroy)
+	
+if __name__ == '__main__':
+	main()
